627b33073a80c9bac5f472b3794438472b0a046b
FLN48 <mr.camcamcam@mail.ru>
2023-10-05 22:46:28 +0300

    +ссылки

--- Source/BearServerAuth/BearServerAuth/ReadMe.txt
+++ Source/BearServerAuth/BearServerAuth/ReadMe.txt
@@ -1,5 +1,7 @@
 0.	Полезные ссылки:
 	-https://metanit.com/sharp/entityframeworkcore/7.1.php
+	-https://habr.com/ru/articles/594971/
+	-https://metanit.com/sharp/aspnet6/13.2.php
 
 1.	Необходимо добавить через Nuget пакеты: 
 	-Npgsql.EntityFrameworkCore.PostgreSQL
---
+++ Source/BearServerAuth/BearServerAuth/SourceClasses/ExceptionHelper.cs
@@ -0,0 +1,47 @@
+﻿namespace BearServerAuth
+{
+    public static class ExceptionHelper
+    {
+        public static string GetFullText(Exception _Exception, bool _NeedStackTrace = false)
+        {
+            if (_Exception == null)
+                return "";
+
+            string errorMessage = "";
+            Exception exc_cur = _Exception;
+            for (int i = 0; i < 20; i++)
+            {
+                string str_cur = "";
+                str_cur += exc_cur.Message + "\r\n";
+
+                errorMessage += str_cur;
+
+                if (exc_cur.InnerException == null)
+                    break;
+
+                exc_cur = exc_cur.InnerException;
+            }
+
+            if (_NeedStackTrace && exc_cur.StackTrace != null)
+                errorMessage += "\r\n" + exc_cur.StackTrace;
+
+            return errorMessage;
+        }
+
+        public static string GetStackTrace(Exception _Exception)
+        {
+            if (_Exception == null)
+                return "";
+
+            Exception exc_cur = _Exception;
+            for (int i = 0; i < 20; i++)
+            {
+                if (exc_cur.InnerException == null)
+                    break;
+                exc_cur = exc_cur.InnerException;
+            }
+
+            return exc_cur.StackTrace ?? "";
+        }
+    }
+}
