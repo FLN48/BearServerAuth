c871c8e44a9a0d60b27d3988ee0d95bf54c86415
FLN48 <mr.camcamcam@mail.ru>
2024-02-29 23:24:34 +0300

    +Начало бокового меню

---
+++ Source/BearServerAuth/BearServerAuth_Razor/Pages/Admin/Admin.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BearServerAuth.Pages.Admin
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+
+    }
+}
