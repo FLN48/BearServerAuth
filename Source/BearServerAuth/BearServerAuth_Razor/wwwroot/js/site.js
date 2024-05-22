4e8fcca3e189ee12717979f797c01d0b3ac57b5e
apotanin <apotanin@simplex48.ru>
2024-05-22 14:46:10 +0300

    +интерфейс рабочий

--- Source/BearServerAuth/BearServerAuth_Razor/wwwroot/js/site.js
+++ Source/BearServerAuth/BearServerAuth_Razor/wwwroot/js/site.js
@@ -6,16 +6,18 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 var state = false;
 function openNav() {
-    var ViewItem = document.getElementById("mySidenav");
     var IconItem = document.getElementById("MenuIcon");
-    if (state) {
-        ViewItem.style.width = "0";
-        state = false;
-    }
-    else {
-        ViewItem.style.width = "250px";
-        state = true;
-    }
+    var ContainerBody = document.getElementById("ContainerBody");
+    //if (state) {
+    //    ViewItem.style.width = "0";
+    //    state = false;
+    //}
+    //else {
+    //    ViewItem.style.width = "250px";
+    //    state = true;
+        
+    //}
+    ContainerBody.classList.toggle("LeftPanel_Open");
     IconItem.classList.toggle("change");
     //document.getElementById("main").style.marginLeft = "250px";
 }
